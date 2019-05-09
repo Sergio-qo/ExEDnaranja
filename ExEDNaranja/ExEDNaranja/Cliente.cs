169b6d9928a9d385173c867267f2f31e14f726d8
ORDENADOR-XAVI\Xavi <xavcanfab@gmail.com>
2019-05-09 15:47:43 +0200

    Añadir Atributos y Constructor

---
+++ ExEDNaranja/ExEDNaranja/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExEDNaranja
+{
+    class Cliente
+    {
+        private string nombre;
+        private string dni;
+        private double saldo;
+
+        public Cliente()
+        {
+            this.saldo = 100;
+        }
+
+
+
+
+
+
+
+    }
+}
